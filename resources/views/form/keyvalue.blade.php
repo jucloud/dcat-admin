4852f896afa688950bca8991bdfe63c09a516b6a
Kai WANG <kai.wang02@united-imaging.com>
2026-02-01 10:58:11 +0800

    修复冗余代码

--- resources/views/form/keyvalue.blade.php
+++ resources/views/form/keyvalue.blade.php
@@ -45,7 +45,7 @@
                         <div class="form-group">
                             <div class="col-sm-12">
                                 <div class="help-block with-errors"></div>
-                                <input name="{{ $name }}[keys][{{ $loop->index }}]" value="{{ $k }}" class="form-control" required/>11
+                                <input name="{{ $name }}[keys][{{ $loop->index }}]" value="{{ $k }}" class="form-control" required/>
                             </div>
                         </div>
                         @endif
@@ -54,7 +54,7 @@
                         <div class="form-group">
                             <div class="col-sm-12">
                                 <div class="help-block with-errors"></div>
-                                <input name="{{ $name }}[values][{{ $loop->index }}]" value="{{ $v }}" class="form-control" />
+                                <input name="{{ $name }}[values][{{ $loop->index }}]" value="{{ $v }}" class="form-control" {!! $attributes !!} />
                             </div>
                         </div>
                     </td>
